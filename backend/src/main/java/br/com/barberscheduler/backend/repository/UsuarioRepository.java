e776d2e966679f7a689d73de6d392ad2fe4a82bf
lucas-oliveirah17 <lucas.oliveirah17@gmail.com>
2025-11-02 16:21:55 -0300

    feat(user): Implement full CRUD for Usuario entity

---
+++ backend/src/main/java/br/com/barberscheduler/backend/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.barberscheduler.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.barberscheduler.backend.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
